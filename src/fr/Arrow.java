ecf185a905e30e0bef952df4f4cdd5342685bed7
julie <julie@PC-JulienS.home>
2018-01-14 14:52:22 +0100

    l

---
+++ src/fr/Arrow.java
@@ -0,0 +1,33 @@
+package fr;
+
+public class Arrow {
+	private float x;
+	private float y;
+	private float length = 10;
+	
+	public Arrow(float x, float y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+	
+	
+	public float getX() {
+		return x;
+	}
+
+	public void setX(float x) {
+		this.x = x;
+	}
+
+	public float getY() {
+		return y;
+	}
+
+	public void setY(float y) {
+		this.y = y;
+	}
+
+	
+	
+}
