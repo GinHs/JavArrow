b601946b21ecbf6e9b5b8c3568b45c682677f240
baptiste <baptiste@baptiste-PC>
2018-01-15 10:42:31 +0100

    construction

--- src/fr/Game.java
+++ src/fr/Game.java
@@ -4,6 +4,13 @@ public class Game {
 	private Character player1;
 	private Character player2;
 	private Character activePlayer;
+	private int deltaX;
+	private int deltaY;
+	private int scorePlayer1;
+	private int scorePlayer2;
+	private JPanel JPanelPlayer1;
+	private JPanel JPanelPlayer2;
+	private Trajectory trajectory;
 	
 	public Game(Character player1, Character player2, Character activePlayer) {
 		this.setPlayer1(player1);
@@ -23,6 +30,66 @@ public class Game {
 		} else {
 			this.setActivePlayer(this.getPlayer1());
 		}
+	}
+	
+	public Trajectory getTrajectory() {
+		return this.trajectory;
+	}
+	
+	public void setTrajectory(Trajectory trajectory) {
+		this.trajectory = trajectory;
+	}
+	
+	public JPanel getJPanelPlayer2() {
+		return this.JPanelPlayer2;
+	}
+	
+	public void setJPanelPlayer2(JPanel JPanelPlayer2) {
+		this.JPanelPlayer2 = JPanelPlayer2;
+	}
+	
+	public JPanel getJPanelPlayer1() {
+		return this.JPanelPlayer1;
+	}
+	
+	public void setJPanelPlayer1(JPanel JPanelPlayer1) {
+		this.JPanelPlayer1 = JPanelPlayer1;
+	}
+	
+	public int getNumManche() {
+		return this.getScorePlayer1() + this.getScorePlayer2();
+	}
+
+	public int getDeltaY() {
+		return this.deltaY;
+	}
+	
+	public void setDeltaY(int deltaY) {
+		this.deltaY = deltaY;
+	}
+	
+	public int getDeltaX() {
+		return this.deltaX;
+	}
+	
+	public void setDeltaX(int deltaX) {
+		this.deltaX = deltaX;
+	}
+
+	public int getScorePlayer2() {
+		return this.scorePlayer2;
+	}
+	
+	public void setScorePlayer2(int scorePlayer2) {
+		this.scorePlayer2 = scorePlayer2;
+	}
+	
+	public int getScorePlayer1() {
+		return this.scorePlayer1;
+	}
+	
+	public void setScorePlayer1(int scorePlayer1) {
+		this.scorePlayer1 = scorePlayer1;
 	}
 	
 	public Character getActivePlayer() {
